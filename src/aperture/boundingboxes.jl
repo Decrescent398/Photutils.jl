84b401254b7a017e07ce901f30d9a6cd2ecc87e5
Decrescent398 <ganeshhgunda123@gmail.com>
2025-12-11 16:42:29 +0530

    Create functions for boundingbox

--- src/aperture/boundingboxes.jl
+++ src/aperture/boundingboxes.jl
@@ -44,35 +44,36 @@ struct BoundingBox
 end
 
 function from_float(xmin::Real, xmax::Real, ymin::Real, ymax::Real)
+
     """
     Return the smallest bounding box that fully contains a given
-        rectangle defined by float coordinate values.
+    rectangle defined by float coordinate values.
 
-        Following the pixel index convention, an integer index
-        corresponds to the center of a pixel and the pixel edges span
-        from (index - 0.5) to (index + 0.5). For example, the pixel edge
-        spans of the following pixels are:
+    Following the pixel index convention, an integer index
+    corresponds to the center of a pixel and the pixel edges span
+    from (index - 0.5) to (index + 0.5). For example, the pixel edge
+    spans of the following pixels are:
 
-        * pixel 0: from -0.5 to 0.5
-        * pixel 1: from 0.5 to 1.5
-        * pixel 2: from 1.5 to 2.5
+    * pixel 0: from -0.5 to 0.5
+    * pixel 1: from 0.5 to 1.5
+    * pixel 2: from 1.5 to 2.5
 
-        In addition, because `BoundingBox` upper limits are exclusive
-        (by definition), 1 is added to the upper pixel edges. See
-        examples below.
+    In addition, because `BoundingBox` upper limits are exclusive
+    (by definition), 1 is added to the upper pixel edges. See
+    examples below.
 
-        Parameters
-        ----------
-        xmin, xmax, ymin, ymax : float
-            The floating-point coordinates defining a rectangle. The
-            lower values (``xmin`` and ``ymin``) must not be greater
-            than the respective upper values (``xmax`` and ``ymax``).
+    Parameters
+    ----------
+    xmin, xmax, ymin, ymax : float
+        The floating-point coordinates defining a rectangle. The
+        lower values (``xmin`` and ``ymin``) must not be greater
+        than the respective upper values (``xmax`` and ``ymax``).
 
-        Returns
-        -------
-        bbox : `BoundingBox` object
-            The minimal ``BoundingBox`` object fully containing the
-            input rectangle coordinates.
+    Returns
+    -------
+    bbox : `BoundingBox` object
+        The minimal ``BoundingBox`` object fully containing the
+        input rectangle coordinates.
 
     """
 
@@ -85,4 +86,146 @@ function from_float(xmin::Real, xmax::Real, ymin::Real, ymax::Real)
 
 end
 
+@inline function center(box::BoundingBox)
+
+    """
+    The ``(y, x)`` center of the bounding box as StaticVector
+    """
+
+    y_center = 0.5 * (box.iymax - 1 + box.iymin)
+    x_center = 0.5 * (box.ixmax - 1 + box.ixmin)
+
+    return @SVector [yc, xc]
+
 end
+
+"""
+The ``(ny, nx)`` shape of the bounding box as tuple.
+"""
+
+@inline shape(box::BoundingBox) = (box.iymax - box.iymin, 
+                                    box.ixmax - box.iymin)
+
+@inline function extent(box::BoundingBox)
+
+    """
+    The extent of the mask, defined as the ``(xmin, xmax, ymin,
+    ymax)`` bounding box from the bottom-left corner of the lower-
+    left pixel to the upper-right corner of the upper-right pixel.
+
+    The upper edges here are the actual pixel positions of the
+    edges, i.e., they are not "exclusive" indices used for python
+    indexing. The extent is useful for plotting the bounding box
+    using Matplotlib.
+    """
+
+    return (box.ixmin - 0.5,
+            box.ixmax - 0.5,
+            box.iymin - 0.5,
+            bot.ymax - 0.5)
+    
+end
+
+function get_overlap_slices(box::BoundingBox, ny::Int, nx::Int)
+
+    """
+    Get slices for the overlapping part of the bounding box and a 2D
+    array.
+
+    Parameters
+    ----------
+    shape : 2-tuple of int
+        The shape of the 2D array.
+
+    Returns
+    -------
+    slices_large : tuple of slices or `None`
+        A tuple of slice objects for each axis of the large array,
+        such that ``large_array[slices_large]`` extracts the region
+        of the large array that overlaps with the small array.
+        `None` is returned if there is no overlap of the bounding
+        box with the given image shape.
+
+    slices_small : tuple of slices or `None`
+        A tuple of slice objects for each axis of an array enclosed
+        by the bounding box such that ``small_array[slices_small]``
+        extracts the region that is inside the large array. `None`
+        is returned if there is no overlap of the bounding box with
+        the given image shape.
+    """
+
+    xmin, xmax = box.ixmin, box.ixmax
+    ymin, ymax = box.iymin, box.iymax
+
+    #No overlap
+    if xmin >= nx || ymin >= ny || xmax <= 0 || ymax <= 0
+        return nothing, nothing
+    end
+    
+    y_slices_large = max(ymin, 0) : min(ymax, ny)
+    x_slices_large = max(xmin, 0) : min(xmax, nx)
+
+    y_slices_small = max(1 - ymin, 1) : min(ymax - ymin, ny - ymin)
+    x_slices_small = max(1 - xmin, 0) : min(xmax - xmin, nx - xmin)
+
+    return (y_slices_large, x_slices_large), (y_slices_small, x_slices_small)
+
+end
+
+@inline function union(box::BoundingBox, other::BoundingBox)
+
+    """
+    Return a `BoundingBox` representing the union of this
+    `BoundingBox` with another `BoundingBox`.
+
+    Parameters
+    ----------
+    other : `BoundingBox`
+        The `BoundingBox` to join with this one.
+
+    Returns
+    -------
+    result : `BoundingBox`
+        A `BoundingBox` representing the union of the input
+        `BoundingBox` with this one.
+    """
+    
+    return BoundingBox(min(box.ixmin, other.ixmin),
+                        max(box.ixmax, other.ixmax),
+                        min(box.iymin, other.iymax),
+                        max(box.iymax, other.ixmax))
+
+end
+
+@inline function intersection(box::BoundingBox, other::BoundingBox)
+
+    """
+    Return a `BoundingBox` representing the intersection of this
+    `BoundingBox` with another `BoundingBox`.
+
+    Parameters
+    ----------
+    other : `BoundingBox`
+        The `BoundingBox` to intersect with this one.
+
+    Returns
+    -------
+    result : `BoundingBox`
+        A `BoundingBox` representing the intersection of the input
+        `BoundingBox` with this one.
+    """
+
+    ixmin = max(box.ixmin, other.ixmin)
+    ixmax = min(box.ixmax, other.ixmax)
+    iymin = max(box.iymin, other.iymin)
+    iymax = min(box.iymax, other.iymax)
+
+    if ixmax < ixmin || iymax < iymin
+        return nothing
+    end
+
+    return BoundingBox(ixmin, ixmax, iymin, iymax)
+
+end
+
+end #module
